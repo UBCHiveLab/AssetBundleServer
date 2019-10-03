5b7cce4f940bb5bae7c9d4cc77fa76a48207cdc0
dacerron <dacerron@gmail.com>
2019-10-03 12:16:36 -0700

    unity scripts included

---
+++ unity_scripts/Editor/BundleBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BundleBuilder : Editor
+{
+    [MenuItem("Assets/Build Asset Bundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("C:\\Users\\Worklearn\\Desktop\\AnatomiVR\\AssetBundles", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+    }
+}
---
+++ unity_scripts/LoadAssetBundles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadAssetBundles : MonoBehaviour
+{
+
+    public string assetName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetAssetBundle());
+    }
+
+    IEnumerator GetAssetBundle()
+    {
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle("http://127.0.0.1:10070/assets/heart");
+        Debug.Log("sending request");
+        yield return www.SendWebRequest();
+
+        if(www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("instantiating");
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+            InstantiateObjectFromBundle(bundle);
+        }
+    }
+
+    void InstantiateObjectFromBundle(AssetBundle bundle)
+    {
+        var prefab = bundle.LoadAsset(assetName);
+        Instantiate(prefab);
+        Debug.Log("finished instantiating");
+    }
+}
